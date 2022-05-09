5ff70bcbe7fec9a80eb79362dd8c655485bb24b4
fernandacastro <fcastro.rj@gmail.com>
2022-05-09 10:15:20 +0200

    02 - ScheduledTasks with SpringTask

---
+++ src/main/java/com/fcastro/catering/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package com.fcastro.catering;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private final CateringJobRepository cateringJobRepository;
+
+    public ScheduledTasks(CateringJobRepository cateringJobRepository){
+        this.cateringJobRepository = cateringJobRepository;
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void reportOrderStats(){
+        LOGGER.info("The total jobs now is {}", cateringJobRepository.count());
+    }
+}
